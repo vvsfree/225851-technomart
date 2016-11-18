ea0d0add4d36e8ac49640aa74d29f0c68b739db5
Vyacheslav <vvsfree@yandex.ru>
2016-11-18 10:52:39 +0600

    Прогрессивное улучшение. Исправлены замечания

--- js/form.js
+++ js/form.js
@@ -1,41 +1,50 @@
 /**
  * Форма обратной связи
  */
-var customerName = document.querySelector("#customer-name");
-var customerEmail = document.querySelector("#customer-email");
-var customerMessage = document.querySelector("#customer-message");
-
-var companyDialog = document.querySelector(".dialog.company-form");
-var companyWriteUsBtn = document.querySelector(".company-write-us");
-companyWriteUsBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-
-  var name = localStorage.getItem("customerName");
-  if (name) {
-    customerName.value = name;
+function form() {
+  var companyDialog = document.querySelector(".dialog.company-form");
+  if (!companyDialog) {
+    // На этой странице нет формы обратной связи
+    return;
   }
 
-  var email = localStorage.getItem("customerEmail");
-  if (email) {
-    customerEmail.value = email;
-  }
+  var customerName = document.querySelector("#customer-name");
+  var customerEmail = document.querySelector("#customer-email");
+  var customerMessage = document.querySelector("#customer-message");
 
-  dialogOverlay.classList.add("display-block");
-  companyDialog.classList.add("display-block");
-
-  // Анимация
-  //companyDialog.classList.add("dialog-animation");
-
-});
-
-var companyDialogForm = companyDialog.querySelector("form");
-companyDialogForm.addEventListener("submit", function(event) {
-  if (customerName.value && customerEmail.value && customerMessage.value) {
-    localStorage.setItem("customerName", customerName.value);
-    localStorage.setItem("customerEmail", customerEmail.value);
-  } else {
-    // Не все поля заполнены.
-    // Все браузеры из ТЗ выделяют незаполненные обязательные (required) поля.
+  var companyWriteUsBtn = document.querySelector(".company-write-us");
+  companyWriteUsBtn.addEventListener("click", function(event) {
     event.preventDefault();
-  }
-});
+
+    var name = localStorage.getItem("customerName");
+    if (name) {
+      customerName.value = name;
+    }
+
+    var email = localStorage.getItem("customerEmail");
+    if (email) {
+      customerEmail.value = email;
+    }
+
+    dialogOverlay.classList.add("display-block");
+    companyDialog.classList.add("display-block");
+
+    // Анимация
+    //companyDialog.classList.add("dialog-animation");
+
+  });
+
+  var companyDialogForm = companyDialog.querySelector("form");
+  companyDialogForm.addEventListener("submit", function(event) {
+    if (customerName.value && customerEmail.value && customerMessage.value) {
+      localStorage.setItem("customerName", customerName.value);
+      localStorage.setItem("customerEmail", customerEmail.value);
+    } else {
+      // Не все поля заполнены.
+      // Все браузеры из ТЗ выделяют незаполненные обязательные (required) поля.
+      event.preventDefault();
+    }
+  });
+}
+
+form();
--- js/map.js
+++ js/map.js
@@ -1,13 +1,25 @@
-var companyMapBtn = document.querySelector(".company-map img");
-var companyMapPopup = document.querySelector(".company-map-popup");
+/**
+ * Интерактивная карта
+ */
+function map() {
+  var companyMapPopup = document.querySelector(".company-map-popup");
+  if (!companyMapPopup) {
+    // На странице нет всплывающего окна с картой
+    return;
+  }
 
-companyMapBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  companyMapPopup.classList.add("display-block");
-});
+  var companyMapBtn = document.querySelector(".company-map img");
 
-var companyMapCloseBtn = companyMapPopup.querySelector(".dialog-close");
-companyMapCloseBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  companyMapPopup.classList.remove("display-block");
-});
+  companyMapBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    companyMapPopup.classList.add("display-block");
+  });
+
+  var companyMapCloseBtn = companyMapPopup.querySelector(".dialog-close");
+  companyMapCloseBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    companyMapPopup.classList.remove("display-block");
+  });
+}
+
+map();
